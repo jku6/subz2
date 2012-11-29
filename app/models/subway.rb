175db78c25daa5256be3245c35ce5a1f75194da6
Jonathan Ku <jonathanku88@gmail.com>
2012-11-29 15:43:01 -0500

    jon

---
+++ app/models/subway.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: subways
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  lat        :float
+#  long       :float
+#  line       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Subway < ActiveRecord::Base
+  attr_accessible :name, :lat, :long, :line
+end
